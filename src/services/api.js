280a1ec6dec6b8e81fa0ce623ddbdc9d76877de3
JuanCM <juancampuzano2356@gmail.com>
2025-04-29 16:35:22 -0500

    fix: obtiene solo la url del archivo .env

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
